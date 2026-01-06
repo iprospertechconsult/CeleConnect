74f84534217dc525796b52f67297b296a2b0f1d7
iPtech <iprosperconsult@gmail.com>
2026-01-06 01:08:10 -0500

    Initial Commit

---
+++ CeleConnect/CeleConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CeleConnectApp.swift
+//  CeleConnect
+//
+//  Created by Deborah on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CeleConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
